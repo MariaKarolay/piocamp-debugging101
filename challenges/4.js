caccce0cf749eb1b4e21b058ce5112f3d0b576c0
Carolina <canobolivarm@gmail.com>
2018-11-26 20:18:10 -0500

    desafio 4

--- challenges/4.js
+++ challenges/4.js
@@ -3,5 +3,5 @@ Corrige los pares faltantes en el código.
 */
 
 let myArray = [1, 2, 3];
-let arraySum = myArray.reduce((previous, current) =>  previous + current);
+let arraySum = myArray.reduce(function (previous, current, index, array) { return previous + current});
 console.log(`Sum of array values is: ${arraySum}`);
